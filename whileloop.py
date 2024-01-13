fe5db6f3b92c76b9285921eabab25906f6a87436
Obashola Immanuel <obasholaimmanuel@Obasholas-MacBook-Air.local>
2024-01-13 16:08:58 -0500

    while loop

---
+++ whileloop.py
@@ -0,0 +1,49 @@
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+# count = 1
+# while count <= 10:
+#     print(count * 3)
+#     count += 1
+
+# for number in range(10):
+#     if number == 5:
+#         break
+#     print(number)
+
+# for number in range(10):
+#     if number == 5:
+#         continue
+#     print(number)
+
+
+count = 1
+while count <= 20:
+    if count % 2 == 0:
+        count += 1
+        continue
+    print(count * 5)
+    count += 1
+
+def is_prime(num):
+    if num <= 1:
+        return False
+
+    divisor = 2
+    while divisor * divisor <= num:
+        if num % divisor == 0:
+            return False
+        divisor += 1
+
+    return True
+
+n = int(input("Enter a number: "))
+
+if is_prime(n):
+    print(" prime number.")
+else:
+    print(" not a prime number.")
+
+
