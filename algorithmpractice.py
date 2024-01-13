dd0ca891c32ae021af4fc2ca59f4987a44f27f95
Obashola Immanuel <obasholaimmanuel@Obasholas-MacBook-Air.local>
2024-01-12 19:39:55 -0500

    class: fibonacci

--- algorithmpractice.py
+++ algorithmpractice.py
@@ -1,39 +1,44 @@
-# def factorial(num):
-#     if num == 1:
+# write a  fibonacci to specific number of terms 
+# write  a program to find the sum of all even fibonnaci numbers 
+# enhance the recursvie fib function w memory 
+
+class fibonacci():
+    def __init__(self,number):
+        self.number = number
+        self.home = []
+
+    def fib(self,number):
+        if number <= 1:
+            return 1
+        else:
+            return (self.fib(number - 1) + self.fib(number - 2))
+
+    def get_fib(self):
+        for i in range(self.number):
+            self.home.append(self.fib(i))
+
+    def even(self):
+        sum = 0 
+        for i in self.home:
+            if i % 2 == 0:
+                sum += i
+        return sum
+
+value = int(input("enter your value: "))
+solution = fibonacci(value)
+
+print(solution.fib(value))
+solution.get_fib()
+print(solution.even())
+
+# def fib_dy(n):
+#     if n == 1 or n == 2:
 #         return 1
-#     else:
-#         return(num * factorial(num - 1))
-
-# print(factorial(10))
-
-# def factorial_calc(num):
-#     result = 1    
-#     for i in range(1, num + 1):
-#         result *= i
-#     return result
-
-# print(factorial_calc(1000))
-
-# def fib(n):
-#     if n <= 1:
-#         return 1
-#     else:
-#         return(fib(n -1)+ fib(n - 2))
-
-# n = int(input("enter your number: "))
-# print("fibonacci sequence: ")
-# for i in range(n):
-#     print(fib(i), end = " ")
-
-def fib_dy(num):
-    if num == 1 or num == 2:
-        return 1
-    ans = [0] * (num + 1)
-    ans[1] = 1
-    ans[2] = 0
-    for i in range(3, num + 1):
-        ans[i] =  ans[i - 1] + ans[i - 2]
-    return ans[num]
-print(fib_dy(1000))
-
-
+#     up_down = [0] * (n + 1)
+#     up_down[1] = 1
+#     up_down[2] = 2
+#     for i in range(3 , n + 1):
+#         up_down[i] = up_down[i - 1] + up_down[i - 2]
+#     return up_down[n]
+    
+# print(fib_dy(100))
