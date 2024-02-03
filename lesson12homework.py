bb16a2a2ae78ed69afc41ba9c0ee4e51d5c8fc41
Obashola Immanuel <obasholaimmanuel@Obasholas-MacBook-Air.local>
2024-02-03 01:48:37 -0500

    class

--- lesson12homework.py
+++ lesson12homework.py
@@ -28,3 +28,35 @@ score = grade_test(student_answers, correct_answers)
 print("Student's score:", score)
 
 
+class TestGrader:
+    def __init__(self, student_answers, correct_answers):
+        self.student_answers = student_answers
+        self.correct_answers = correct_answers
+
+    def grade_test(self):
+        score = 0
+        for question_num in self.student_answers:
+            student_choices = self.student_answers[question_num]
+            correct_choices = self.correct_answers.get(question_num, [])
+
+            if student_choices == correct_choices:
+                score += 1
+        return score
+
+
+
+student_answers = {
+    1: ['A', 'B'],
+    2: ['C'],
+    3: ['A', 'C', 'D']
+}
+
+correct_answers = {
+    1: ['A', 'B'],
+    2: ['C', 'D'],
+    3: ['A', 'C', 'D']
+}
+
+test_grader = TestGrader(student_answers, correct_answers)
+score = test_grader.grade_test()
+print("Student's score:", score)
